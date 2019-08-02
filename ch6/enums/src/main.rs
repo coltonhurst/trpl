52227845bad1b12b35fd502495c1c256ec56751a
coltonhurst <colton@coltonhurst.com>
2019-08-02 17:56:59 -0400

    started ch6

---
+++ ch6/enums/src/main.rs
@@ -0,0 +1,28 @@
+// Defining Enums in Rust
+
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn main() {
+    // making some enum vals
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    // making structs that have the enum
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+}
